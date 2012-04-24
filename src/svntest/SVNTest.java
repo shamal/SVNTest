2b1706e89f111b5c228b4d2103938c622c7ca9df
Shamal Karunarathne <shamalk@gmail.com>
2012-04-23 18:58:07 -0700

    Initial Commit from netbeans

---
+++ src/svntest/SVNTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package svntest;
+
+/**
+ *
+ * @author shamal
+ */
+public class SVNTest
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
